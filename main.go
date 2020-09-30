f19994579d3fe23111c452058e5aabc52e6f902b
youzhian <youzhian@aliyun.com>
2020-09-30 11:16:10 +0800

    增加controllor

--- main.go
+++ main.go
@@ -10,35 +10,36 @@ import (
 	"overview/web/middleware"
 )
 
-func main()  {
-	app :=iris.New()
+func main() {
+	app := iris.New()
 	app.Logger().SetLevel("debug")
 	//app.Use(recover.New())
 	//app.Use(logger.New())
 
 	// 加载视图模板地址
-	app.RegisterView(iris.HTML("./web/views",".html"))
+	app.RegisterView(iris.HTML("./web/views", ".html"))
 	// 注册控制器
 	// mvc.New(app.Party("/movies")).Handle(new(controllers.MovieController))
 	//你也可以使用  `mvc.Configure` 方法拆分编写 MVC 应用程序的配置。
 	// 如下所示：
-	mvc.Configure(app.Party("/movies"),movies)
+	mvc.Configure(app.Party("/movies"), movies)
+	mvc.Configure(app.Party("/hello"), hello)
 
 	app.Run(
-			// Start the web server at localhost:8080
-			iris.Addr(":8080"),
-			// skip err server closed when CTRL/CMD+C pressed:
-			iris.WithoutServerError(iris.ErrServerClosed),
-			// enables faster json serialization and more:
-			iris.WithOptimizations,
-		)
+		// Start the web server at localhost:8080
+		iris.Addr(":8080"),
+		// skip err server closed when CTRL/CMD+C pressed:
+		iris.WithoutServerError(iris.ErrServerClosed),
+		// enables faster json serialization and more:
+		iris.WithOptimizations,
+	)
 }
 
 // 注册控制器
 // mvc.New(app.Party("/movies")).Handle(new(controllers.MovieController))
 //你也可以使用  `mvc.Configure` 方法拆分编写 MVC 应用程序的配置。
 // 如下所示：
-func movies(app *mvc.Application){
+func movies(app *mvc.Application) {
 	// Add the basic authentication(admin:password) middleware
 	// for the /movies based requests.
 	app.Router.Use(middleware.BasicAuth)
@@ -52,4 +53,8 @@ func movies(app *mvc.Application){
 	// 注意，你可以初始化多个控制器
 	// 你也可以 使用 `movies.Party(relativePath)` 或者 `movies.Clone(app.Party(...))` 创建子应用。
 	app.Handle(new(controllors.MovieControllor))
+}
+
+func hello(app *mvc.Application) {
+	app.Handle(new(controllors.HelloControllor))
 }
--- web/controllors/helloControllor.go
+++ web/controllors/helloControllor.go
@@ -1,1 +1,20 @@
 package controllors
+
+type HelloControllor struct {
+}
+
+func (h *HelloControllor) Get() string {
+	return "this is HelloControllor !"
+}
+
+//生成的请求地址是sayhello/{param:string}
+func (h *HelloControllor) GetSayhelloBy(name string) string {
+	//name := cxt.FormValue("name")
+	return "hello " + name + " !"
+}
+
+//生成url的规则是，根据驼峰命名法将方法名先分成一个数组，判断第一个值是否合规（Get、Post、Any、All...)，
+//判断是否使用了By，by后边会直接跟着参数
+func (h *HelloControllor) PostSayhelloBy(name string) string {
+	return "hello " + name + " !"
+}
